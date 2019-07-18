f190766d61ca8d2270b78921c667e7083aa46e80
Jacary Richardson <jr@localhost.localdomain>
2019-07-18 17:54:04 -0400

    Updates

---
+++ precache-manifest.b5d8d8312ba3ccf9da2cf4faba33aaeb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6a62f62b46f040b9d148",
+    "url": "/static/js/main.a26d998b.chunk.js"
+  },
+  {
+    "revision": "8b18a53dbb725788516c",
+    "url": "/static/js/2.5844aa6b.chunk.js"
+  },
+  {
+    "revision": "6a62f62b46f040b9d148",
+    "url": "/static/css/main.f30146e8.chunk.css"
+  },
+  {
+    "revision": "f7aca3baa0ae00f5a7e310d1d2e5fcd0",
+    "url": "/index.html"
+  }
+];
